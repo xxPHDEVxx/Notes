2ab0b40e5f35c2f1c52ada9470d157c01d0ce60c
Jamila H <Jamila-h@outlook.fr>
2023-12-04 12:39:24 +0100

    build: lien bootstrap et page index + ControllerMain

--- controller/ControllerMain.php
+++ controller/ControllerMain.php
@@ -1,8 +1,8 @@
 <?php
 require_once "framework/Controller.php";
 
-class ControllerTest extends Controller {
+class ControllerMain extends Controller {
     public function index() : void {
-        echo "<h1>Hello !</h1>";
+        (new View("index"))->show();
     }
 }
