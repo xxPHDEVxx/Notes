98fae05a802f768f0013ecfa16fa6aa7626188a4
rayben <rayanbenaissa@gmail.com>
2024-01-28 19:32:48 +0100

    feat(settings): ajustement style

--- view/view_settings.php
+++ view/view_settings.php
@@ -7,14 +7,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 </head>
 <body>
     <div class = "title"><h1>Settings</h1></div>
     <div id="settings_div"> <h2>Hey <?= $user->full_name ?> ! </h2></div>
     <div class = "menu_settings">
-        <a href="view_editProfile">Edit Profile</a>
-        <a href="view_changePassword">Change password</a>
-        <a href="view_login">Logout</a>
+        <a href="view_editProfile"><i class="fas fa-pen"></i>Edit Profile</a>
+        <a href="view_changePassword"><i class="fas fa-key"></i>Change password</a>
+        <a href="view_login"><i class="fas fa-door-open"></i>Logout</a>
     </div>
 
     <?php (new View("menu"))->show(); ?>
