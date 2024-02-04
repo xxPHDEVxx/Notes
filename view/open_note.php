ab873f4587cfc5a67d3461fde6cf59708221b723
Aouatif Hamouch <0101aohamouch@student.epfc.eu>
2024-02-04 15:03:17 +0100

    fix bug: refactorer le lien de archive et unarchive

--- view/open_note.php
+++ view/open_note.php
@@ -5,7 +5,7 @@
         <a class="back" href="<?=$_SESSION['previous_page']?>"><span class="material-symbols-outlined">arrow_back_ios</span></a> 
         <?php if($archived == 1):?>
             <a class="delete" href="#"><span class="material-symbols-outlined" id ="delete_icon">delete_forever</span></a>
-            <a class="unarchive" href="note/unarchive/<?=$note_id?>"><span class="material-symbols-outlined">unarchive</span></a>
+            <a class="unarchive" href="openNote/unarchive/<?=$note_id?>"><span class="material-symbols-outlined">unarchive</span></a>
            
         
         <?php elseif($isShared_as_editor == 1): ?>
@@ -19,7 +19,7 @@ push_pin
             <?php else: ?>
                 <a class="pinned" href="openNote/pin/<?=$note_id?>"><span class="material-symbols-outlined">push_pin</span></a>
             <?php endif; ?>    
-            <a class="archive" href="note/archive/<?=$note_id?>"><span class="material-symbols-outlined">archive</span></a>
+            <a class="archive" href="openNote/archive/<?=$note_id?>"><span class="material-symbols-outlined">archive</span></a>
             <a class="isShared" href="#"><span class="material-symbols-outlined">edit</span></a>
         <?php endif; ?>    
      
