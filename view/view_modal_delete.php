d82ad39265cad54f6f1b7f4dd985e90c51331691
Jamila H <jamila-h@outlook.fr>
2024-04-21 22:07:28 +0200

    fix: contenu du bouton delete

--- view/view_modal_delete.php
+++ view/view_modal_delete.php
@@ -11,7 +11,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-        <button type="button" class="btn btn-danger" id="confirmExitButton">Quitter sans sauvegarder</button>
+        <button type="button" class="btn btn-danger" id="confirmExitButton">Yes, delete it !</button>
       </div>
     </div>
   </div>
