0c88d4dfa9adfcb84917a58b6ace88e88c400642
rayben <rayanbenaissa@gmail.com>
2024-05-31 18:21:39 +0200

    feat_B : ajout méthodes traitements base de données sans le visuel

--- JS/edit_checklist_errors.js
+++ JS/edit_checklist_errors.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
         saveButton.disabled = true;
 
 
-    // ******Gestionnaire d'évènements****** 
+    /* ******Gestionnaire d'évènements****** */
 
     // Ajout des gestionnaires d'événements pour les éléments de contenu
     itemContents.forEach(function (itemContent) {
@@ -25,7 +25,10 @@ $(document).ready(function () {
             // Vérification du contenu de l'élément
             checkContent(itemContent, itemId, errorSpan);
         });
+
     });
+
+
 
     // Ajout d'un gestionnaire d'événements pour le titre
     titleInput.addEventListener('input', checkTitle);
@@ -35,9 +38,42 @@ $(document).ready(function () {
         checkNewContent(newItemTag);
     });
 
+    $('.icone-add').click(function (event) {
+        event.preventDefault(); // Empêche le comportement par défaut du formulaire (soumission)
+        addNewContent(newItemTag);
+    });
+
+    $('.icone-delete').click(function (event) {
+        event.preventDefault(); // Empêche le comportement par défaut du formulaire (soumission)
+
+        // Récupération de l'ID de l'élément
+        let buttonId = this.id;
+        let itemIdMatch = buttonId.match(/delete(\d+)/); // Extraction de l'ID
+        if (itemIdMatch) {
+            id = itemIdMatch[1];
+            deleteContent(id); // Appelle la fonction pour supprimer l'élément
+        }
+    });
+
+    $('#saveButton').click(function (event) {
+        event.preventDefault(); // Empêche le comportement par défaut du formulaire (soumission)
+        addTitle();
+
+        // Parcours de tous les éléments de contenu
+        itemContents.forEach(function (itemContent) {
+            // Récupération de l'ID de l'élément
+            let name = itemContent.getAttribute('name');
+            let itemIdMatch = name.match(/\[(\d+)\]/); // Extraction de l'ID
+            let itemId = itemIdMatch[1];
+            // Récupération de l'élément d'erreur correspondant
+            // Ajout du contenu de l'élément
+            saveContent(itemContent, itemId);
+        });
+    });
 
 
-    // *****Méthodes*****
+
+    /* *****Méthodes***** */
 
     // Fonction pour mettre à jour l'état du bouton de sauvegarde en fonction de la validité du titre
     function updateSaveButtonState() {
@@ -109,6 +145,26 @@ $(document).ready(function () {
         });
     }
 
+    function saveContent(itemInput, itemId) {
+        // Envoi d'une requête AJAX pour vérifier si le contenu de la note est valide
+        var requestData = {
+            items: itemInput.value,
+            id: itemId
+        };
+
+        $.ajax({
+            url: "note/save_content_checklist_service", // L'URL où envoyer la requête
+            type: "POST", // Méthode de la requête (POST)
+            data: requestData, // Les données à envoyer (le titre de la note)
+            success: function (data) { // Fonction exécutée en cas de succès de la requête
+                console.log(data);
+            },
+            error: function (xhr, status, error) { // Fonction exécutée en cas d'erreur de la requête
+                console.error("Erreur lors de l'ajout du contenu de la note : ", error); // Affichage de l'erreur dans la console
+            }
+        });
+    }
+
     function checkNewContent(newItem) {
         // Envoi d'une requête AJAX pour vérifier si le contenu de la note est valide
         var requestData = {
@@ -151,6 +207,47 @@ $(document).ready(function () {
         });
     }
 
+    function addNewContent(newItem) {
+        // Envoi d'une requête AJAX pour vérifier si le contenu de la note est valide
+        var requestData = {
+            new: newItem.value,
+            note_id: note
+        };
+
+        $.ajax({
+            url: "note/add_new_content_checklist_service", // L'URL où envoyer la requête
+            type: "POST", // Méthode de la requête (POST)
+            data: requestData, // Les données à envoyer (le titre de la note)
+            success: function () {
+                let newItemData = JSON.parse(data);
+                // Créer l'élément HTML à ajouter à la vue
+                var newItemHTML = '<div class="item">' + newItemData.content + '</div>';
+            },
+            error: function (xhr, status, error) { // Fonction exécutée en cas d'erreur de la requête
+                console.error("Erreur lors de la vérification du contenu de la note : ", error); // Affichage de l'erreur dans la console
+            }
+        });
+    }
+
+    function deleteContent(itemId) {
+        // Envoi d'une requête AJAX pour vérifier si le contenu de la note est valide
+        var requestData = {
+            id: itemId
+        };
+
+        $.ajax({
+            url: "note/delete_item_service", // L'URL où envoyer la requête
+            type: "POST", // Méthode de la requête (POST)
+            data: requestData, // Les données à envoyer (le titre de la note)
+            success: function (data) { // Fonction exécutée en cas de succès de la requête
+                console.log(data);
+            },
+            error: function (xhr, status, error) { // Fonction exécutée en cas d'erreur de la requête
+                console.error("Erreur lors de l'ajout du contenu de la note : ", error); // Affichage de l'erreur dans la console
+            }
+        });
+    }
+
     // Fonction pour vérifier le titre
     function checkTitle() {
         // Envoi d'une requête AJAX pour vérifier si le titre de la note est valide
@@ -178,6 +275,27 @@ $(document).ready(function () {
                     titleInput.classList.add('is-valid');
                     updateSaveButtonState();
                 }
+            },
+            error: function (xhr, status, error) { // Fonction exécutée en cas d'erreur de la requête
+                console.error("Erreur lors de la vérification du titre unique : ", error); // Affichage de l'erreur dans la console
+            }
+        });
+    }
+
+    // Fonction pour vérifier le titre
+    function addTitle() {
+        // Envoi d'une requête AJAX pour vérifier si le titre de la note est valide
+        var requestData = {
+            title: titleInput.value,
+            note: note // Assurez-vous que note est défini
+        };
+
+        $.ajax({
+            url: "note/add_title_service", // L'URL où envoyer la requête
+            type: "POST", // Méthode de la requête (POST)
+            data: requestData, // Les données à envoyer (le titre de la note)
+            success: function () { // Fonction exécutée en cas de succès de la requête
+
             },
             error: function (xhr, status, error) { // Fonction exécutée en cas d'erreur de la requête
                 console.error("Erreur lors de la vérification du titre unique : ", error); // Affichage de l'erreur dans la console
