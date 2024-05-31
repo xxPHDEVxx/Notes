003f858ab37d75ca270719f2b77b2d8767a17e3b
Jamila H <jamila-h@outlook.fr>
2024-05-31 15:23:26 +0200

    label sur les pages my archives et shared by

--- view/view_shared_notes.php
+++ view/view_shared_notes.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" >
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0">
     <link href="css/style.css" rel="stylesheet" type="text/css">
 </head>
 
@@ -25,6 +25,14 @@
                         <a class="link-note-archivee" href="note/open_note/<?= $note_item["id"] ?>">
                             <?php include("note_in_list.php") ?>
                         </a>
+                        <div class="box-label">
+                            <?php $note = Note::get_note_by_id($note_item["id"]);
+                            foreach ($note->get_labels() as $label) : ?>
+                                <span class="badge label-note text-bg-secondary"><?= $label ?></span>
+
+                            <?php endforeach; ?>
+                        </div>
+
                     </div>
                 <?php endforeach; ?>
             </div>
@@ -37,6 +45,14 @@
                         <a class="link-note-archivee" href="note/open_note/<?= $note_item["id"] ?>">
                             <?php include("note_in_list.php") ?>
                         </a>
+                        <div class="box-label">
+                            <?php $note = Note::get_note_by_id($note_item["id"]);
+                            foreach ($note->get_labels() as $label) : ?>
+                                <span class="badge label-note text-bg-secondary"><?= $label ?></span>
+
+                            <?php endforeach; ?>
+                        </div>
+
                     </div>
 
                 <?php endforeach; ?>
